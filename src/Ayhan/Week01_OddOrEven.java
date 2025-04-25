5c0c3688dd089ede0b70616be607bf6ddf8138f8
Ayhi777 <ayhan7bayram@gmail.com>
2025-04-25 21:01:33 +0100

    Created ready files for all users!!!

--- src/Ayhan/Week01_OddOrEven.java
+++ src/Ayhan/Week01_OddOrEven.java
@@ -4,5 +4,20 @@ public class Week01_OddOrEven {
 
     public static void main(String[] args) {
 
+        oddOrEven(241);
+        oddOrEven(21);
+        oddOrEven(541);
+
+
     }
+    public static void oddOrEven(int num){
+        if (num%2==0){
+            System.out.println(num+" is a even number");
+        }else if (num%2==1){
+            System.out.println(num+" is a odd number");
+        }else {
+            System.out.println(num+" is a zero");
+        }
+    }
+
 }
