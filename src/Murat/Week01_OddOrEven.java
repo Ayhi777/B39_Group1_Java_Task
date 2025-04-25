3f4ca57d04d53654f3aad985f94f727560c6f985
muratvarcin <cozumkumesiofficial@gmail.com>
2025-04-25 21:40:39 +0300

    Main and oddOrEven methods are created.

---
+++ src/Murat/Week01_OddOrEven.java
@@ -0,0 +1,20 @@
+package Murat;
+
+public class Week01_OddOrEven {
+    public static void main(String[] args) {
+        oddOrEven(20);
+
+    }
+
+
+    public static void oddOrEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num +" is an Even Number.");
+        }else if(num % 2 == 1){
+            System.out.println(num +" is an Odd Number.");
+        }else {
+            System.out.println(num +" is zero.");
+        }
+    }
+
+}
