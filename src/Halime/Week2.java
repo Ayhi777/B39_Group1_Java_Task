1277f418c77c38d0c89ed98eb1fd60f4d1208457
Halimecakir <ckrhalime@hotmail.com>
2025-04-29 20:34:41 +0300

    Implement reverseString method to reverse input strings 2nd way

--- src/Halime/Week2.java
+++ src/Halime/Week2.java
@@ -2,31 +2,7 @@ package Halime;
 
 public class Week2 {
     public static void main(String[] args) {
-//
-//        String str = "apple";
-//
-//        String reversed = "";
-//
-//        for (int i = str.length() - 1; i >= 0; i--) {
-//
-//            reversed += str.charAt(i);
-//
-//        }
 
-//        System.out.println(reversed);
-
-
-        String str = "apple";
-
-        String reversed = "";
-
-        for (int i = str.length() - 1; i >= 0; i--) {
-
-            reversed += str.charAt(i);
-
-        }
-
-        System.out.println(reversed);
 
     }
 
