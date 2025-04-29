00e4162bb15c9f402f8e6387d8395695e4b21a79
Halimecakir <ckrhalime@hotmail.com>
2025-04-29 20:35:27 +0300

    Implement reverseString method to reverse input strings delete

--- src/Halime/Week2.java
+++ src/Halime/Week2.java
@@ -4,6 +4,18 @@ public class Week2 {
     public static void main(String[] args) {
 
 
+        String str = "apple";
+
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            reversed.append(str.charAt(i));
+
+        }
+
+        System.out.println(reversed);
+
     }
 
 }
