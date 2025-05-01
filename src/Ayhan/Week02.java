7a848292d588ace599b772111c318b8ad4914fed
Ayhi777 <ayhan7bayram@gmail.com>
2025-05-01 12:08:14 +0100

    Created ready files for all users!!!

--- src/Ayhan/Week02.java
+++ src/Ayhan/Week02.java
@@ -1,4 +1,21 @@
 package Ayhan;
 
 public class Week02 {
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= 30; i++){
+            System.out.print(finra(i) + " ");
+        }
+    }
+    public static String finra(int num){
+        if(num % 3 == 0 && num % 5 == 0){
+            return "FINRA";
+        }else if(num % 3 == 0){
+            return "FIN";
+        }else if(num % 5 == 0){
+            return "RA";
+        }else{
+            return String.valueOf(num);
+        }
+    }
 }
