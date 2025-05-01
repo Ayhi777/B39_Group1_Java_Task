e7eea7369fa0b8e575cfe7ab538f72464a2b7176
Filiz-Sa <filizaydiniz@hotmail.com>
2025-05-01 15:46:46 +0300

    Deleted Practice class and added Finra class.

--- src/Filiz/Week02_Finra.java
+++ src/Filiz/Week02_Finra.java
@@ -2,10 +2,11 @@ package Filiz;
 
 public class Week02_Finra {
     public static void main(String[] args) {
-writeFinra(1,30);
+        writeFinra(1,30);
 
-    }
-    public static void writeFinra(int start,int end){
+
+
+    }public static void writeFinra(int start,int end){
         while (start<=end){
 
             if (start%3==0 && end%5==0){
@@ -20,5 +21,5 @@ writeFinra(1,30);
             start++;
         }
     }
+    }
 
-}
