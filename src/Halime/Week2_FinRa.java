cd924f8cd60d126d0d3d9c5c53501deb841fce3a
Halimecakir <ckrhalime@hotmail.com>
2025-05-02 08:59:53 +0300

    Method updated

---
+++ src/Halime/Week2_FinRa.java
@@ -0,0 +1,26 @@
+package Halime;
+
+public class Week2_FinRa {
+    public static void main(String[] args) {
+        printFinRa(1, 30);
+
+    }
+
+
+    public static void printFinRa(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FinRa");
+            } else if (i % 3 == 0) {
+                System.out.println("Fin");
+            } else if (i % 5 == 0) {
+                System.out.println("Ra");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+
