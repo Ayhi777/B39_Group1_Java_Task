efa2587929c2cd9b97ac17bf5392138b4c56b3e9
Halimecakir <ckrhalime@hotmail.com>
2025-05-09 08:10:46 +0300

    FINRA method is updated (09.05)

--- src/Halime/Week2_FinRa.java
+++ src/Halime/Week2_FinRa.java
@@ -1,26 +1,47 @@
 package Halime;
 
 public class Week2_FinRa {
+
+
     public static void main(String[] args) {
         printFinRa(1, 30);
+        printFinRa(5, 25);
 
     }
 
 
     public static void printFinRa(int num1, int num2) {
-        for (int i = 1; i <= num2; i++) {
-            if (i % 15 == 0) {
-                System.out.print("FinRa" + " ");
-            } else if (i % 3 == 0) {
-                System.out.print("Fin" + " ");
-            } else if (i % 5 == 0) {
-                System.out.print("Ra" + " ");
+        while (num1 <= num2) {
+            if (num1 % 15 == 0) {
+                System.out.print(" FinRa ");
+            } else if (num1 % 5 == 0) {
+                System.out.print(" Ra ");
+            } else if (num1 % 3 == 0) {
+                System.out.print(" Fin ");
             } else {
-                System.out.print(i + " ");
+                System.out.print(" " + num1);
             }
+            num1++;
         }
+        System.out.println();
     }
 }
+//        for (int num1 = 1; num1 <= num2; num1++) {
+//            if (num1 % 15 == 0) {
+//                System.out.print("FinRa" + " ");
+//            } else if (num1 % 3 == 0) {
+//                System.out.print("Fin" + " ");
+//            } else if (num1 % 5 == 0) {
+//                System.out.print("Ra" + " ");
+//            } else {
+//                System.out.print(num1 + " ");
+//
+//            }
+//
+//        }
+//        System.out.println();
+
+
 
 
 
