e57ab71b65b15a6199434478fe2b066d57474da7
muratvarcin <cozumkumesiofficial@gmail.com>
2025-07-07 17:40:26 +0300

    added the new class named Password_Validation_Task

---
+++ src/Murat/Week09_Password_Validation_Task.java
@@ -0,0 +1,64 @@
+package Murat;
+
+public class Week09_Password_Validation_Task {
+
+
+    public static void main(String[] args) {
+        // Test cases
+        System.out.println("----------------------------------------");
+        System.out.println(isValidPassword("Abc1@")); // false, too short
+        System.out.println("----------------------------------------");
+        System.out.println(isValidPassword("Abcdef")); // false, no digit or special char
+        System.out.println("----------------------------------------");
+        System.out.println(isValidPassword("Abc1@dKl!")); // true
+        System.out.println("----------------------------------------");
+        System.out.println(isValidPassword("abc1@k")); // false, no uppercase
+        System.out.println("----------------------------------------");
+        System.out.println(isValidPassword("ABC1!DE")); // false, no lowercase
+        System.out.println("----------------------------------------");
+        System.out.println(isValidPassword("!@Abc1 ")); // false, contains space
+        System.out.println("-----------------NEGATIVE TEST-----------------------");
+        System.out.println(isValidPassword("Abc1@dKlAAAAAAAAAAAAAAbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee111111111111111111111111111111111111122222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222")); // false ?
+    }
+
+    public static boolean isValidPassword(String password){
+        if(password.length() < 6 && !password.contains(" ")) {
+
+            boolean hasUpperCase = false;
+            boolean hasLowerCase = false;
+            boolean hasDigit = false;
+            boolean hasSpecialChar = false;
+
+            for (char c : password.toCharArray()) {
+                if (Character.isUpperCase(c)) {
+                    hasUpperCase = true;
+                } else if (Character.isLowerCase(c)) {
+                    hasLowerCase = true;
+                } else if (Character.isDigit(c)) {
+                    hasDigit = true;
+                } else if (!Character.isLetterOrDigit(c)) {
+                    hasSpecialChar = true;
+                }
+            }
+
+            return hasUpperCase && hasLowerCase && hasDigit && hasSpecialChar;
+
+
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    String -- Password Validation Task
+1. Write a return method that can verify if a password is valid or not.
+requirements:
+ 1. Password MUST be at least have 6 characters and should not contain space
+2. PassWord should at least contain one upper case letter
+3. PassWord should at least contain one lowercase letter
+4. Password should at least contain one special characters
+5. Password should at least contain a digit
+
+if all requirements above are met, the method returns true, otherwise returns  false
+     */
+}
